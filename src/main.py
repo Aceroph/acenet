1c6bdec756a8e387f6248a33957a4a19abec6f8a
Aceroph <109217147+Aceroph@users.noreply.github.com>
2023-03-16 15:37:54 -0400

    added contact page

--- src/main.py
+++ src/main.py
@@ -4,4 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('base.html')
+	return render_template('base.html')
+
+@app.route('/contact')
+def contact():
+	return '<h1 style="text-align: center;">Contact me pls</h1>'
